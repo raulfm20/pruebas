cdce1a7ab91bc4b046fa7342a916e00e5efb3d96
raulfm20 <44257695+raulfm20@users.noreply.github.com>
2018-10-18 10:51:27 +0200

    Update TestAnimal.java

--- TestAnimal.java
+++ TestAnimal.java
@@ -22,7 +22,7 @@ public class TestAnimal {
 		animal1.setEdad(edad);
 		//Informar de los datos de animal1
 		animal1.nace();
-		System.out.println("Me llamo " + animal1.getNombre() + " y tengo " + animal1.getEdad() + " años");
+		System.out.println("Mi nomre es " + animal1.getNombre() + " y mi edad es de " + animal1.getEdad() + " años");
 	}
 
 }
